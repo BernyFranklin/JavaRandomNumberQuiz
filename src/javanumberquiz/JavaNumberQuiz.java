cd3a3392bdde9c1d559c6f5c42fa9ab710498bb4
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-17 16:43:40 -0700

    Created and tested gradeQuiz() works 4.0. Moving on to error handling

--- src/javanumberquiz/JavaNumberQuiz.java
+++ src/javanumberquiz/JavaNumberQuiz.java
@@ -27,6 +27,10 @@ public class JavaNumberQuiz {
     };
     // Declare constant for quiz length
     private static final int QUIZ_LENGTH = 10;
+    // Declare constant for max score an question points
+    private static final int MAX_SCORE = 100;
+    private static final int CORRECT_ANSWER = 10;
+    
     
 
     public static void main(String[] args) {
@@ -34,11 +38,14 @@ public class JavaNumberQuiz {
         createQuiz();
         // Call administerQuiz
         administerQuiz();
+        // Call gradeQuiz
+        gradeQuiz();
     }   // End of PSV Main
     private static void createQuiz() {
         // Loop through 0-9 to fill arrays
         for (int counter = 0; counter < QUIZ_LENGTH; counter++) {
             // Generate numbers between 0 - 99 to fill arrays
+            // 100 used to select number 0 - 99
             num1[counter] = (int)(Math.random()*100);
             num2[counter] = (int)(Math.random()*100);
         }   // End of for loop
@@ -62,4 +69,33 @@ public class JavaNumberQuiz {
         }   // End of for loop
         System.out.println ("=========================================");
     }   // End of administerQuiz
+    
+    // Start gradeQuiz
+    private static void gradeQuiz() {
+        // Define variables for details on grade
+        int pointCount = 0;
+        int score = 0;
+        // Results title
+        System.out.println ("              Final Results              ");
+        System.out.println ("=========================================");
+        // Loop through questions and answers of quiz
+        for (int counter = 0; counter < QUIZ_LENGTH; counter++) {
+            // Print original problem with original answer
+            System.out.printf ("\n%2d. %2d + %2d = %3d\n",
+                    (counter + 1), num1[counter], num2[counter], answer[counter]);
+            if (answer[counter] == (num1[counter] + num2[counter])) {
+                System.out.printf ("                  CORRECT");
+                pointCount++;
+            } else {
+                System.out.printf ("                  INCORRECT, Answer is %3d", 
+                        (num1[counter] + num2[counter]));
+            }  // End of if/else
+        }   // End of for loop
+        // Compute score by multiplying pointCount by CORRECT_ANSWER
+        score = pointCount * CORRECT_ANSWER;
+        // Print score
+        System.out.printf ("\n==========================================");
+        System.out.printf ("\n        Your final score is %2d/%3d\n", score, MAX_SCORE);
+        System.out.printf ("==========================================\n");
+    }   // End of gradeQuiz
 }   // End of Class JavaNumberQuiz
