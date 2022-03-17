519a1187eda8b6185e4d147bab3d99435c2e7bfc
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-17 15:09:21 -0700

    Shell built

--- src/javanumberquiz/JavaNumberQuiz.java
+++ src/javanumberquiz/JavaNumberQuiz.java
@@ -1,20 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * JavaNumberQuiz Version1.0
+ * Frank Bernal
+ * CIS 084 Java Programming
+ * Input: Answers
+ * Output: Graded Quiz
+ * 17 Mar 2022
  */
 package javanumberquiz;
 
-/**
- *
- * @author frankbernal
- */
 public class JavaNumberQuiz {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    }   // End of PSV Main
     
-}
+}   // End of Class JavaNumberQuiz
