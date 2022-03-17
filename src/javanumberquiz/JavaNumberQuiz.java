cbb871f736d1d9f7ad43bafaabaa9eb7b52b5a66
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-17 15:40:47 -0700

    Imported Scanner and Math Defined arrays num1, num2, and answers Created createQuiz() method createQuiz(0 uses Math.random to fill arays num1 and num2

--- src/javanumberquiz/JavaNumberQuiz.java
+++ src/javanumberquiz/JavaNumberQuiz.java
@@ -7,11 +7,31 @@
  * 17 Mar 2022
  */
 package javanumberquiz;
+// Import scanner for user input
+import java.util.Scanner;
+// Import Math for random number gen
+import java.lang.Math;
 
 public class JavaNumberQuiz {
+    // Declare num1[] for first number in problem
+    private static int[ ] num1 = {10};
+    // Declare num2[] for second number in problem
+    private static int[ ] num2 = {10};
+    // Declare answer[] for answers input by user
+    private static int[ ] answer = {10};
+    // Declare constant for quiz length
+    private static final int QUIZ_LENGTH = 10;
+    
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Call createQuiz
+        createQuiz();
     }   // End of PSV Main
-    
+    private static void createQuiz() {
+        for (int counter = 0; counter < QUIZ_LENGTH; counter++) {
+            // Generate numbers between 0 - 99 to fill arrays
+            num1[counter] = (int)(Math.random() * 100);
+            num2[counter] = (int)(Math.random() * 100);
+        }
+    }
 }   // End of Class JavaNumberQuiz
